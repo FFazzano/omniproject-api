3f3f5531d59f96c360b3ff31d0696a52e2febde8
Felipe Fazzano <felipefazzano@gmail.com>
2026-03-18 01:09:38 -0300

    fix: update CORS configuration to use the correct frontend URL for deployment

--- API/src/main/java/com/omniproject/API/config/WebConfig.java
+++ API/src/main/java/com/omniproject/API/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // Libera todas as rotas da API
                 .allowedOrigins(
                         "http://localhost:5173", // URL do seu React local (Vite)
-                        "https://seu-projeto.vercel.app" // Substitua pela URL real do seu front-end na Vercel
+                        "https://omniproject-frontend.vercel.app" // Substitua pela URL real do seu front-end na Vercel
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*") // Permite todos os cabeçalhos (incluindo o Authorization)
